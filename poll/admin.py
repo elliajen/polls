2b7131a1453859242eb4e50e9e4a5cdf903fdab5
elliajen <dmstndhk123@naver.com>
2021-10-07 12:00:05 +0900

    설문

--- poll/admin.py
+++ poll/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # 클래스 테이블(객체) 등록
 admin.site.register(Question)
+admin.site.register(Choice)
--- poll/views.py
+++ poll/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from poll.models import Question
 
-# 127.0.0.1:8000/poll
+#127.0.0.1:8000/poll - 전체 목록
 def index(request):
-    return HttpResponse("안녕하세요~ django")
+    question_list = Question.objects.all
+    return render(request, 'poll/index.html', {'question_list': question_list})
 
-#127.0.0.1:8000/poll/1
+    #return HttpResponse("안녕하세요~ django")
+
+#127.0.0.1:8000/poll/1 - 1개 정보
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    question = Question.objects.get(id=question_id)
+    return render(request, 'poll/detail.html', {'question': question})
+
+    #return HttpResponse("You are looking at question %s." % question_id)
