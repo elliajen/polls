6b30bb5a0ce10da4482747ba269ea7dfe6471005
elliajen <dmstndhk123@naver.com>
2021-10-08 12:58:29 +0900

    설문

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from poll import views
 
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('poll/', views.index),
-    path('poll/<int:question_id>/', views.detail)
+    path('poll/', include('poll.urls'))
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,17 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from poll.models import Question
 
-#127.0.0.1:8000/poll - 전체 목록
+# 127.0.0.1:8000/poll - 전체 목록
 def index(request):
     question_list = Question.objects.all
     return render(request, 'poll/index.html', {'question_list': question_list})
 
-    #return HttpResponse("안녕하세요~ django")
+    # return HttpResponse("안녕하세요~ django")
 
-#127.0.0.1:8000/poll/1 - 1개 정보
+# 127.0.0.1:8000/poll/1 - 1개 정보
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     return render(request, 'poll/detail.html', {'question': question})
 
-    #return HttpResponse("You are looking at question %s." % question_id)
+    # return HttpResponse("You are looking at question %s." % question_id)
+
+# 127.0.0.1:8000/poll/2/vote
+def vote(request, question_id):
+    try:
+        question = Question.objects.get(id=question_id)
+        choice = request.POST['choice'] # name에서 가져오기
+        sel_choice = question.choice_set.get(id=choice)
+    except KeyError:
+        return render(request,'poll/detail.html', {'question': question, 'error_message': '선택을 해주세요'})
+    else:
+        sel_choice.votes += 1
+        sel_choice.save()
+        return HttpResponseRedirect(reverse('poll:result',args=(question.id,)))
+
+# 127.0.0.1:8000/poll/2/results
+def result(request, question_id):
+    question = Question.objects.get(id=question_id)
+    return render(request, 'poll/result.html', {'question':question})
